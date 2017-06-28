683a6cd9bc384b01ae3545eb52db65e48672e054
kkotov <kkotov@cern.ch>
2017-06-28 13:22:07 +0200

    synch

--- RandomForest.h
+++ RandomForest.h
@@ -516,12 +516,32 @@ public:
 public:
     double regress(const DataRow& row) const {
         double sum = 0.;
-        for(const auto &tree : ensemble)
-            sum += tree.predict(row).asFloating;
+        for(const auto &tree : ensemble){
+            Variable v = tree.predict(row);
+            if( v.type != Variable::Continuous )
+                return 0; // maybe throw?
+            sum += v.asFloating;
+        }
         return sum/ensemble.size();
     }
 
-    int classify(const DataRow& row) const { return 0; }
+    int classify(const DataRow& row) const {
+        std::unordered_map<long long, size_t> votes;
+        for(const auto &tree : ensemble){
+            Variable v = tree.predict(row);
+            if( v.type != Variable::Categorical )
+                return 0; // maybe throw?
+            votes[ v.asIntegral ]++;
+        }
+        std::unordered_map<long long, size_t>::const_iterator it =
+            std::max_element(votes.cbegin(),
+                             votes.cend(),
+                             [](std::pair<long long, size_t> a, std::pair<long long, size_t> b){
+                                 return a.second < b.second;
+                             }
+            );
+        return it->first;
+    }
 
     void train(const DataFrame& df, const std::vector<unsigned int>& predictorsIdx, unsigned int responseIdx) {
         if( df.nrow() < 1 ) return ;
