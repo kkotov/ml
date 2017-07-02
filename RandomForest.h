77654ad3aaf18561c0c41e52bc62aaa3ad2d80af
kkotov <kkotov@cern.ch>
2017-07-02 20:35:17 +0200

    Rearrangement

--- RandomForest.h
+++ RandomForest.h
@@ -1,5 +1,5 @@
-#ifndef RandomForest_h
-#define RandomForest_h
+#ifndef TreeTrainer_h
+#define TreeTrainer_h
 
 #include <map>
 #include <list>
@@ -16,9 +16,8 @@
 #include "DataFrame.h"
 #include "Tree.h"
 
-class RandomForest {
+class TreeTrainer {
 private:
-public:
     typedef std::list<std::pair<unsigned int,unsigned int>> SplitVars; // variable index and level (if categorical)
 
     std::default_random_engine rState;
@@ -65,13 +64,14 @@ public:
     // thread-safe implementation of CART with gini/entropy/rms purity metrices
     Tree* findBestSplits(const DataFrame& df,
                          unsigned int responseIdx,
-                         const SplitVars& vars,
-                         const std::vector<unsigned int>& subset = {}
+                         const std::vector<unsigned int>& predictorsIdx,
+                         const std::vector<unsigned int>& subset = {},
+                         bool  isRandomForest = true
     ){
         Tree *tree = new Tree();
 
         // safety: nothing to split on? - return an empty tree
-        if( vars.empty() ) return tree;
+        if( predictorsIdx.empty() ) return tree;
 
         size_t size = subset.size();
         tree->set_size = size;
@@ -128,7 +128,7 @@ public:
 
         // find best split
         size_t    bestSplitVar = 0;
-        long long bestSplitPoint = 0; // integral split points to a row for continuous vars and to a level for factors
+        long long bestSplitPoint = 0; // integral split points to a row for continuous predictors and to a level for factors
         double    bestSplitMetric = std::numeric_limits<double>::max();
 
         // functional form of Eq 9.13 on p.307 of ESLII (*_l - left to the cut, *_r - right)
@@ -153,6 +153,29 @@ public:
                        return (size_l ? double(gini_l)/size_l/size_l : 0) +
                               (size_r ? double(gini_r)/size_r/size_r : 0);
                 };
+
+        // the only Random-Forest specific step here: draw a random subset of predictors
+        SplitVars vars;
+        if( isRandomForest ){
+            vars = generateRandomSplitVars(
+                      df.getSchema(),
+                      predictorsIdx,
+                      ( df.getLevels(responseIdx).size() == 0 ?
+                          std::floor(predictorsIdx.size()>15 ? predictorsIdx.size()/3 : 5) :
+                          std::floor( sqrt(predictorsIdx.size()) )
+                      )
+                   );
+        } else {
+            for(unsigned int idx : predictorsIdx){
+                if( df.getLevels(idx).size() == 0 )
+                    vars.push_back(std::make_pair(idx,0));
+                else {
+                    // one-HOT encoding
+                    for(long level : df.getLevels(idx))
+                        vars.push_back(std::make_pair(idx,level));
+                }
+            }
+        }
 
         // loop over split candidates, greedily select the best one
         for(std::pair<unsigned int,unsigned int> var : vars){
@@ -281,8 +304,8 @@ public:
         if( left_subset.size() > MIN_ENTRIES && right_subset.size() > MIN_ENTRIES ){
 
             // another good place to use the threads
-            Tree *left_subtree  = findBestSplits(df, responseIdx, vars, left_subset);
-            Tree *right_subtree = findBestSplits(df, responseIdx, vars, right_subset);
+            Tree *left_subtree  = findBestSplits(df, responseIdx, predictorsIdx, left_subset,  isRandomForest);
+            Tree *right_subtree = findBestSplits(df, responseIdx, predictorsIdx, right_subset, isRandomForest);
 
             left_subtree->parent  = tree;
             right_subtree->parent = tree;
@@ -314,158 +337,128 @@ public:
         return tree;
     }
 
-    std::vector<Tree> ensemble;
-
 public:
-    double regress(const DataRow& row) const {
-        double sum = 0.;
-        for(const auto &tree : ensemble){
-            Variable v = tree.predict(row);
-            if( v.type != Variable::Continuous )
-                return 0; // maybe throw?
-            sum += v.asFloating;
-        }
-        return sum/ensemble.size();
-    }
 
-    int classify(const DataRow& row) const {
-        std::unordered_map<long long, size_t> votes;
-        for(const auto &tree : ensemble){
-            Variable v = tree.predict(row);
-            if( v.type != Variable::Categorical )
-                return 0; // maybe throw?
-            votes[ v.asIntegral ]++;
+    // train one simple Classification And Regression Tree
+    std::shared_ptr<Tree> trainCART(const DataFrame& df, const std::vector<unsigned int>& predictorsIdx, unsigned int responseIdx, long seed) {
+        if( df.nrow() < 1 ) return ;
+
+        // reproducibility
+        rState.seed(seed);
+
+        // cross-validate models over nFolds for evaluating the best model complexity
+        const size_t nFolds = 10;
+        std::vector<double> alphas[nFolds]; // series of hyper-parameter for each fold
+        std::vector<std::shared_ptr<Tree>> models[nFolds]; // series of corresponding models for each fold
+        std::vector<double> outOfSampleError[nFolds]; // OOS error estimate
+        std::vector<unsigned int> shuffled = sample(df.nrow(),df.nrow());
+        for(size_t fold=0; fold<nFolds; fold++){
+            // partition into training and validation sets
+            std::vector<unsigned int> trainSet, validSet;
+            trainSet.reserve( shuffled.size() );
+            validSet.reserve( shuffled.size() );
+            std::vector<unsigned int>::const_iterator begin = shuffled.cbegin() + (shuffled.size()*fold)/nFolds;
+            std::vector<unsigned int>::const_iterator   end = shuffled.cbegin() + (shuffled.size()*(fold+1))/nFolds;
+            std::copy(shuffled.cbegin(), begin,           std::back_inserter(trainSet));
+            std::copy(end,               shuffled.cend(), std::back_inserter(trainSet));
+            std::copy(begin,             end,             std::back_inserter(validSet));
+            // get the tree
+            Tree *tree = findBestSplits(df, responseIdx, predictorsIdx, trainSet, false);
+            // and prune it
+            std::map<double,std::shared_ptr<Tree>> m = tree->prune();
+            alphas[fold].reserve( m.size() );
+            models[fold].reserve( m.size() );
+            outOfSampleError[fold].reserve( m.size() );
+            // remember
+            //  hyper-parameters
+            std::transform(m.cbegin(),
+                           m.cend(),
+                           std::back_inserter(alphas[fold]),
+                           [](std::pair<double,std::shared_ptr<Tree>> a){
+                               return a.first;
+                           }
+            );
+            //  corresponding models
+            std::transform(m.cbegin(),
+                           m.cend(),
+                           std::back_inserter(models[fold]),
+                           [](std::pair<double,std::shared_ptr<Tree>> a){ 
+                               return a.second;
+                           }
+            );
+            //  and their out-of-sample errors
+            std::transform(m.cbegin(),
+                           m.cend(),
+                           std::back_inserter(outOfSampleError[fold]),
+                           [&df, &responseIdx, &validSet](std::pair<double,std::shared_ptr<Tree>> a){
+                               return a.second->evaluateMetric(df, responseIdx, validSet);
+                           }
+            );
         }
-        std::unordered_map<long long, size_t>::const_iterator it =
-            std::max_element(votes.cbegin(),
-                             votes.cend(),
-                             [](std::pair<long long, size_t> a, std::pair<long long, size_t> b){
+
+        // among different model complexities pick one
+        //  "...to minimize the average error." (slide #20 of 08-trees-handout.pdf of StatLearn)
+        std::map<double,double> averageError;
+        // run over alpha hyper-parameter that controls model complexity
+        size_t pos[nFolds] = {};
+        while(1){
+            // find fold holding the next smallest alpha
+            double nextAlpha = std::numeric_limits<double>::max();
+            int nextFold = -1;
+            for(size_t fold=0; fold<nFolds; fold++){
+                if( pos[fold] == alphas[fold].size() - 1 ) continue;
+                if( nextAlpha > alphas[fold][ pos[fold] + 1 ] ){
+                    nextAlpha = alphas[fold][ pos[fold] + 1 ]; 
+                    nextFold  = fold;
+                }
+            }
+            // no more alphas remain
+            if( nextFold < 0 ) break;
+
+            pos[nextFold]++;
+
+            double aveErr = 0;
+            for(size_t fold=0; fold<nFolds; fold++)
+                aveErr += outOfSampleError[fold][ pos[fold] ];
+            aveErr /= nFolds;
+
+            averageError.insert(std::make_pair(nextAlpha,aveErr));
+        }
+
+        std::map<double,double>::const_iterator it =
+            std::min_element(averageError.begin(),
+                             averageError.end(),
+                             [](std::pair<double,double> a, std::pair<double,double> b){
                                  return a.second < b.second;
                              }
             );
-        return it->first;
+
+        double bestAlpha = it->first;
+
+        Tree *tree = findBestSplits(df, responseIdx, predictorsIdx, shuffled, false);
+        std::map<double,std::shared_ptr<Tree>> m = tree->prune();
+        auto model = m.lower_bound(bestAlpha);
+
+        return model->second;
     }
 
-    void train(const DataFrame& df, const std::vector<unsigned int>& predictorsIdx, unsigned int responseIdx) {
-        if( df.nrow() < 1 ) return ;
+    std::vector<std::shared_ptr<Tree>> trainRandomForest(const DataFrame& df, const std::vector<unsigned int>& predictorsIdx, unsigned int responseIdx, long seed) {
         // reproducibility
-        rState.seed(0);
+        rState.seed(seed);
+
+        std::vector<std::shared_ptr<Tree>> ensemble;
+
         const int nTrees = 1;
         for(unsigned int t=0; t<nTrees; t++){
-            SplitVars vars(
-                generateRandomSplitVars(
-                    df.getSchema(),
-                    predictorsIdx,
-                    ( df.getLevels(responseIdx).size() == 0 ?
-                        std::floor(predictorsIdx.size()>15 ? predictorsIdx.size()/3 : 5) :
-                        std::floor( sqrt(predictorsIdx.size()) )
-                    )
-                )
-            );
-//for(auto s : vars) std::cout << "s.first = "<<s.first << " s.second = "<< s.second << std::endl;
 
-            // cross-validate models over nFolds for evaluating the best model complexity
-            const size_t nFolds = 10;
-            std::vector<double> alphas[nFolds]; // series of hyper-parameter for each fold
-            std::vector<std::shared_ptr<Tree>> models[nFolds]; // series of corresponding models for each fold
-            std::vector<double> outOfSampleError[nFolds]; // OOB error estimate
-            std::vector<unsigned int> shuffled = sample(df.nrow(),df.nrow());
-            for(size_t fold=0; fold<nFolds; fold++){
-                // partition into training and validation sets
-                std::vector<unsigned int> trainSet, validSet;
-                trainSet.reserve( shuffled.size() );
-                validSet.reserve( shuffled.size() );
-                std::vector<unsigned int>::const_iterator begin = shuffled.cbegin() + (shuffled.size()*fold)/nFolds;
-                std::vector<unsigned int>::const_iterator   end = shuffled.cbegin() + (shuffled.size()*(fold+1))/nFolds;
-                std::copy(shuffled.cbegin(), begin,           std::back_inserter(trainSet));
-                std::copy(end,               shuffled.cend(), std::back_inserter(trainSet));
-                std::copy(begin,             end,             std::back_inserter(validSet));
-                // get the tree
-                Tree *tree = findBestSplits(df, responseIdx, vars, trainSet);
-                // and prune it
-                std::map<double,std::shared_ptr<Tree>> m = tree->prune();
-                alphas[fold].reserve( m.size() );
-                models[fold].reserve( m.size() );
-                outOfSampleError[fold].reserve( m.size() );
-                // remember
-                //  hyper-parameters
-                std::transform(m.cbegin(),
-                               m.cend(),
-                               std::back_inserter(alphas[fold]),
-                               [](std::pair<double,std::shared_ptr<Tree>> a){
-                                   return a.first;
-                               }
-                );
-                //  corresponding models
-                std::transform(m.cbegin(),
-                               m.cend(),
-                               std::back_inserter(models[fold]),
-                               [](std::pair<double,std::shared_ptr<Tree>> a){ 
-                                   return a.second;
-                               }
-                );
-                //  and their out-of-sample errors
-                std::transform(m.cbegin(),
-                               m.cend(),
-                               std::back_inserter(outOfSampleError[fold]),
-                               [&df, &responseIdx, &validSet](std::pair<double,std::shared_ptr<Tree>> a){
-                                   return a.second->evaluateMetric(df, responseIdx, validSet);
-                               }
-                );
-            }
-
-            // among different model complexities pick one
-            //  "...to minimize the average error." (slide #20 of 08-trees-handout.pdf of StatLearn)
-            std::map<double,double> averageError;
-            // run over alpha hyper-parameter that controls model complexity
-            size_t pos[nFolds] = {};
-            while(1){
-                // find fold holding the next smallest alpha
-                double nextAlpha = std::numeric_limits<double>::max();
-                int nextFold = -1;
-                for(size_t fold=0; fold<nFolds; fold++){
-                    if( pos[fold] == alphas[fold].size() - 1 ) continue;
-                    if( nextAlpha > alphas[fold][ pos[fold] + 1 ] ){
-                        nextAlpha = alphas[fold][ pos[fold] + 1 ]; 
-                        nextFold  = fold;
-                    }
-                }
-                // no more alphas remain
-                if( nextFold < 0 ) break;
-
-                pos[nextFold]++;
-
-                double aveErr = 0;
-                for(size_t fold=0; fold<nFolds; fold++)
-                    aveErr += outOfSampleError[fold][ pos[fold] ];
-                aveErr /= nFolds;
-
-                averageError.insert(std::make_pair(nextAlpha,aveErr));
-            }
-
-            std::map<double,double>::const_iterator it =
-                std::min_element(averageError.begin(),
-                                 averageError.end(),
-                                 [](std::pair<double,double> a, std::pair<double,double> b){
-                                     return a.second < b.second;
-                                 }
-                );
-
-            double bestAlpha = it->first;
-std::cout << "bestAlpha=" << bestAlpha << std::endl;
-
-            Tree *tree = findBestSplits(df, responseIdx, vars, shuffled);
-            std::map<double,std::shared_ptr<Tree>> m = tree->prune();
-            auto model = m.lower_bound(bestAlpha);
+        }
 
 //model->second->save(std::cout);
-            ensemble.push_back( std::move(*(model->second)) );
-        }
+//        ensemble.push_back( std::move(*()) );
+
+        return ensemble;
     }
 
-    RandomForest(void){}
-
-//    load/save
 };
 
 #endif
